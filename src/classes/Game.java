4f6c1dcb222dc912860abe5af26434cdf2f498e5
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2022-11-20 03:09:43 +0100

    Game

--- src/classes/Game.java
+++ src/classes/Game.java
@@ -5,41 +5,55 @@ import classes.ConsoleColors;
 
 public class Game {
 
-
-	
 	ConsoleColors consolecolor = new ConsoleColors();
-	
 
 	public static void Play(String word)
 	{
 		Scanner input = new Scanner(System.in);
+		int rounds=1;
+		int gueses=0;
+		int max_rounds=10;
 		System.out.println(ConsoleColors.PURPLE_BOLD + word +" \n");
 		char[] wordC = word.toCharArray();
 		char[] solutionC = {'*','*','*','*','*'};
 		System.out.println("Raad het woord bestaande uit vijf letters");
 		System.out.println("* * * * *");
-		
-		System.out.println("Geen een woord van vijf tekens in: ");
-		String inputString = (input.nextLine().toString());
-		while (inputString.length()!=5){
-		System.out.println("Geen een woord van vijf tekens in: ");
-		inputString = (input.nextLine().toString());
-		}
-		
-		char[] inputC = inputString.toCharArray();
-		for(int i = 0;i<wordC.length;i++)
+
+
+		do
 		{
-			if(inputC[i]==wordC[i])
-			{
-				solutionC[i]=wordC[i];
+			gueses=0;
+			System.out.println("\n| Kans "+rounds+" van "+max_rounds+" | Geef een woord van vijf letters in: ");
+			String inputString = (input.nextLine().toString());
+			while (inputString.length()!=5){
+				System.out.println("| Kans "+rounds+" van "+max_rounds+" | Geef een woord van vijf letters in: ");
+				inputString = (input.nextLine().toString());
 			}
+
+			char[] inputC = inputString.toCharArray();
+			for(int i = 0;i<wordC.length;i++)
+			{
+				if(inputC[i]==wordC[i])
+				{
+					solutionC[i]=wordC[i];
+					gueses++;
+
+				}
+			}
+			System.out.print("Reeds geraden: ");
+			for(int i =0;i<solutionC.length;i++)
+			{
+				System.out.print(solutionC[i]);
+			}
+			System.out.println("");
+			rounds++;
+
 		}
-		
-		for(int i =0;i<solutionC.length;i++)
+		while(gueses!=wordC.length && rounds<=max_rounds);
+		if(gueses ==5)
 		{
-			System.out.print(solutionC[i]);
+			System.out.println("Proficiat, je hebt het woord volledig geraden");
 		}
-		
 	}
 
 
