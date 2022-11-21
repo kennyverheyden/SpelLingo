5eb4aa30f3e8a8c3b2cd69e86c075f609db4920a
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2022-11-21 01:04:08 +0100

    Game classe

--- src/classes/Game.java
+++ src/classes/Game.java
@@ -1,5 +1,6 @@
 package classes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import classes.ConsoleColors;
 
@@ -11,50 +12,104 @@ public class Game {
 	{
 		Scanner input = new Scanner(System.in);
 		int rounds=1;
-		int gueses=0;
 		int max_rounds=10;
-		System.out.println(ConsoleColors.PURPLE_BOLD + word +" \n");
-		char[] wordC = word.toCharArray();
-		char[] solutionC = {'*','*','*','*','*'};
+		char[] wordC = word.toUpperCase().toCharArray(); // Te raden woord (opsplitsen in letters)
+		char[] existingC = {' ',' ',' ',' ',' '}; //Bijhouden geraden letters die in het woord voorkomen maar op een andere positie
+		char[] solutionC = {'*','*','*','*','*'};//Bijhouden geraden oplossing, letters op juiste plaats = woord
+		boolean win=false; //Spel beëndigen wanneer het woord is geraden
+		boolean existingCharCheck = false; //Bijhouden of minstens een letter van andere positie werd geraden voor sysout melding
+		
+		//System.out.println(ConsoleColors.BLUE_BOLD + word +" \n"); //Oplossing
 		System.out.println("Raad het woord bestaande uit vijf letters");
 		System.out.println("* * * * *");
 
 
-		do
+		do // zolang rondes (rounds) en niet gewonnen (win)
 		{
-			gueses=0;
-			System.out.println("\n| Kans "+rounds+" van "+max_rounds+" | Geef een woord van vijf letters in: ");
-			String inputString = (input.nextLine().toString());
+			// Geef antwoord in
+			System.out.println(ConsoleColors.PURPLE_BOLD +"\n| Kans "+rounds+" van "+max_rounds+" | Geef een woord van vijf letters in: "+ConsoleColors.RESET);
+			String inputString = (input.nextLine().toString().toUpperCase());
 			while (inputString.length()!=5){
-				System.out.println("| Kans "+rounds+" van "+max_rounds+" | Geef een woord van vijf letters in: ");
+				System.out.println(ConsoleColors.PURPLE_BOLD + "\n Voer een geldig vijf letter woord in:"+ConsoleColors.RESET);
 				inputString = (input.nextLine().toString());
 			}
 
+			// Controleer antwoord
 			char[] inputC = inputString.toCharArray();
 			for(int i = 0;i<wordC.length;i++)
 			{
 				if(inputC[i]==wordC[i])
 				{
-					solutionC[i]=wordC[i];
-					gueses++;
+					solutionC[i]=wordC[i]; //Oplossing bijhouden
 
 				}
 			}
-			System.out.print("Reeds geraden: ");
-			for(int i =0;i<solutionC.length;i++)
-			{
-				System.out.print(solutionC[i]);
-			}
-			System.out.println("");
-			rounds++;
 
+			// Controleer of er bestaande letters in het woord zitten op een andere plaats
+			for(int i = 0;i<wordC.length;i++)
+			{
+				for(int j=0;j<wordC.length;j++)
+				{
+					if(inputC[i]==wordC[j])
+					{
+						if(inputC[i]!=solutionC[j])
+						{
+							existingC[i] = inputC[i];
+							existingCharCheck=true; //Bijhouden of minstens een letter van andere positie geraden voor sysout melding
+						}
+					}
+				}
+			}
+
+			if(Arrays.equals(solutionC, wordC))
+			{
+				win=true; // Gewonnen, doorbreek de while loop
+			}
+			else
+			{
+				// Reeds geraden letters weergeven van de juiste positie
+				System.out.print(ConsoleColors.GREEN_BOLD+"\n| "+inputString.toLowerCase()+" | Geraden: ");
+				for(int i =0;i<solutionC.length;i++)
+				{
+					System.out.print(ConsoleColors.BLUE_BOLD+solutionC[i]+" "+ConsoleColors.RESET);
+				}
+				System.out.print(ConsoleColors.GREEN_BOLD + "|"+ConsoleColors.RESET);
+
+				// Bestaande letters weergeven die in het woord voorkomen
+				if(existingCharCheck)
+				{
+					System.out.print(ConsoleColors.YELLOW_BOLD+" Andere plaats: ");
+					for(int i=0;i<existingC.length;i++)
+					{
+						if(existingC[i]!=' ')
+						{
+							System.out.print(existingC[i]+" ");
+						}
+					}
+					//bestaande letters van andere plaats wissen
+					for(int i=0;i<existingC.length;i++)
+					{
+						existingC[i]=' ';
+					}
+					existingCharCheck=false;
+				}
+				else
+				{
+					System.out.println(ConsoleColors.GREEN_BOLD +" Geen andere plaats letters "+ConsoleColors.RESET);
+				}
+
+				rounds++; // Gespeelde ronde
+			}
 		}
-		while(gueses!=wordC.length && rounds<=max_rounds);
-		if(gueses ==5)
+		while(win!=true && rounds<=max_rounds);
+
+		if(win)
 		{
-			System.out.println("Proficiat, je hebt het woord volledig geraden");
+			System.out.println(ConsoleColors.BLUE_BOLD +" \nProficiat, je hebt het woord: "+word+" volledig geraden"+ConsoleColors.RESET);
+		}
+		else
+		{
+			System.out.println(ConsoleColors.BLUE_BOLD +" \nJammer, je hebt het woord: "+word+" niet geraden"+ConsoleColors.RESET);
 		}
 	}
-
-
 }
